cf18948e8cdd3951e61905bb83b82001b591f131
Sean-Youfa <sean@yourfutureacademy.nl>
2023-09-22 02:38:23 +0200

    20230921 finished project

--- private/classes/parsecsv.class.php
+++ private/classes/parsecsv.class.php
@@ -2,55 +2,92 @@
 
     class ParseCSV {
 
-        public $filename;
-        private $header;
-        private $data=[];
+        private static $filename;
+        public static $delimiter = ',';
+        private static $header;
+        private static $data=[];
+        private static $row_count = 0;
 
-        public function __construct($filename = ""){
-            if($filename != ""){
-                $this -> filename = $filename;
+        public function __construct($filename = "", $delimiter = ','){
+            static::file($filename);
+        }
+
+        public static function file($filename = ""){
+
+            if($filename == ""){
+                return false;
             }
+            else if(!file_exists($filename)){
+                return false;
+            }
+            else if(!is_readable($filename)){
+                return false;
+            }
+
+            static::$filename = $filename;
+            
+            return true;
         }
 
         public static function parse($filename = ""){
             
+            static::reset();
+
+            $valid_file = true;
+
             if($filename != ""){
-                $this -> filename = $filename;
+                $valid_file = static::file($filename);
+            }      
+
+            if(!$valid_file){
+                return static::$data;
             }
 
-            $data = [];
-            $file = fopen($this -> filename, 'r');
-
-            // $this -> header = false;
+            $file = fopen(static::$filename, 'r');
     
             while(!feof($file)){
-              $row =  fgetcsv($file, 0, ',');
+              $row =  fgetcsv($file, 0, static::$delimiter);
               if($row == [NULL] || $row === false){continue;}
-                if(!$this -> header){
+                if(!static::$header){
                   foreach($row as $value){
-                    $this -> header[$value] = array_search($value, $row);
+                    static::$header[$value] = array_search($value, $row);
                   }
                 }
                 else{           
     
                   $args = [
-                    'brand' =>  $row[$header['brand']],
-                    'model' =>  $row[$header['model']],
-                    'year'  =>  $row[$header['year']],
-                    'category' => $row[$header['category']],
-                    'color' =>  $row[$header['color']],
-                    // 'description' => $row[$header['description']],
-                    'gender' => $row[$header['gender']],
-                    'set_weight' => $row[$header['weight_kg']],
-                    'set_price' => $row[$header['price']],
-                    'set_condition' => $row[$header['condition_id']],
+                    'brand' =>  $row[static::$header['brand']],
+                    'model' =>  $row[static::$header['model']],
+                    'year'  =>  $row[static::$header['year']],
+                    'category' => $row[static::$header['category']],
+                    'color' =>  $row[static::$header['color']],
+                    // 'description' => $row[static::$header['description']],
+                    'gender' => $row[static::$header['gender']],
+                    'set_weight' => $row[static::$header['weight_kg']],
+                    'set_price' => $row[static::$header['price']],
+                    'set_condition' => $row[static::$header['condition_id']],
                   ];
                 
-                  $this -> data[] = $args;
+                  static::$data[] = $args;
+                  static::$row_count++;
                 }
             }
             fclose($file);
-            return $this -> data;
+            return static::$data;
+        }
+
+        public static function result(){
+            return static::$data;  
+        }
+
+        public static function row_count(){
+            return static::$row_count;    
+        }
+
+        public static function reset(){
+            static::$header = NULL;  
+            static::$data = [];  
+            static::$row_count = 0;      
         }
     }
 
--- public/bicycles.php
+++ public/bicycles.php
@@ -1,48 +1,6 @@
 <?php require_once('../private/initialize.php'); ?>
 
 <?php $page_title = 'Inventory'; ?>
-<?php 
-
-  $bicycle_input = [
-    'brand' =>  'Gazelle',
-    'model' =>  'E-Bike',
-    'year'  =>  2023,
-    'set_catergory' => 5,
-    'color' =>  'Gray',
-    'description' => 'Latest E-Bike from the Gazelle Brand.',
-    'set_gender' => 1,
-    'set_weight' => 12.5,
-    'set_price' => "$1399,95",
-    'set_condition' => 4,
-  ];
-  
-  $bicycle = NEW Bicycle($bicycle_input);
-
-  // $object_vars = get_object_vars($bicycle);
-  // echo "<pre>";
-  //     print_r($object_vars);
-  // echo "</pre>";
-  
-  // echo $bicycle -> get_price()."<br/>";
-  // echo $bicycle -> get_weight_lbs()."<br/>";
-  // echo $bicycle -> get_condition()."<br/>";
-
-  // echo "<hr />";
-
-  // $unicycle = NEW Unicycle($bicycle_input);
-
-  // $object_vars = get_object_vars($unicycle);
-  // echo "<pre>";
-  //     print_r($object_vars);
-  // echo "</pre>";
-
-
-  // echo $unicycle -> get_price()."<br/>";
-  // echo $unicycle -> get_weight_lbs()."<br/>";
-  // echo $unicycle -> get_condition()."<br/>";
-
-  // echo $unicycle -> slogan()."<br/>";
-?>
 
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
@@ -71,56 +29,10 @@
 
       <?php 
 
-        $data = [];
-        // $file = fopen(PRIVATE_PATH."/used_bicycles.csv", 'r');
- 
-        // $getHeader = false;
-        // $header = false;
-        // // $header = [
-        // //   'brand' =>  'Gazelle',
-        // //   'model' =>  'E-Bike',
-        // //   'year'  =>  2023,
-        // //   'category' => 5,
-        // //   'color' =>  'Gray',
-        // //   // 'description' => 'Latest E-Bike from the Gazelle Brand.',
-        // //   'gender' => 1,
-        // //   'weight_kg' => 12.5,
-        // //   'price' => "$1399,95",
-        // //   'condition_id' => 4,
-        // // ];
+        $data = ParseCSV::parse(PRIVATE_PATH."/used_bicycles.csv"); 
 
-        // while(!feof($file)){
-        //   $row =  fgetcsv($file, 0, ',');
-        //   if($row == [NULL] || $row === false){continue;}
-        //     if(!$header){
-        //       foreach($row as $value){
-        //         $header[$value] = array_search($value, $row);
-        //       }
-        //     }
-        //     else{           
-
-        //       $args = [
-        //         'brand' =>  $row[$header['brand']],
-        //         'model' =>  $row[$header['model']],
-        //         'year'  =>  $row[$header['year']],
-        //         'category' => $row[$header['category']],
-        //         'color' =>  $row[$header['color']],
-        //         // 'description' => $row[$header['description']],
-        //         'gender' => $row[$header['gender']],
-        //         'set_weight' => $row[$header['weight_kg']],
-        //         'set_price' => $row[$header['price']],
-        //         'set_condition' => $row[$header['condition_id']],
-        //       ];
-            
-        //       $data[] = NEW Bicycle($args);
-        //     }
-        // }
-
-        // fclose($file);
-        $data = ParseCSV::parse(PRIVATE_PATH."/used_bicycles.csv");  
-
-        foreach($data as $arg){
-          $bicycle = NEW Bicycle($arg)
+        foreach($data as $args){
+          $bicycle = NEW Bicycle($args)
       ?>
       <tr>
           <td><?php echo h($bicycle->brand);?></td>
